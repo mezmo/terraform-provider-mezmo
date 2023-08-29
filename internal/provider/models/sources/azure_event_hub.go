7db8f6118e5c5b5b64cec2326d869fa9cf7f2567
Dan Hable <dan@danhable.com>
2023-08-29 13:36:18 -0500

    feat(source): adding azure event hub source
    Add support for Azure Event Hub source and unit test for new code.
    
    Ref: LOG-17850

---
+++ internal/provider/models/sources/azure_event_hub.go
@@ -0,0 +1,122 @@
+package sources
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	. "github.com/hashicorp/terraform-plugin-framework/types"
+	. "github.com/mezmo-inc/terraform-provider-mezmo/internal/client"
+	. "github.com/mezmo-inc/terraform-provider-mezmo/internal/provider/models/modelutils"
+)
+
+type AzureEventHubSourceModel struct {
+	Id               String `tfsdk:"id"`
+	PipelineId       String `tfsdk:"pipeline_id"`
+	Title            String `tfsdk:"title"`
+	Description      String `tfsdk:"description"`
+	GenerationId     Int64  `tfsdk:"generation_id"`
+	Decoding         String `tfsdk:"decoding"`
+	ConnectionString String `tfsdk:"connection_string"`
+	Namespace        String `tfsdk:"namespace"`
+	GroupId          String `tfsdk:"group_id"`
+	Topics           List   `tfsdk:"topics"`
+}
+
+func AzureEventHubSourceResourceSchema() schema.Schema {
+	return schema.Schema{
+		Description: "Represents an Azure Event Hub source.",
+		Attributes: ExtendBaseAttributes(map[string]schema.Attribute{
+			"decoding": schema.StringAttribute{
+				Required:    false,
+				Optional:    true,
+				Computed:    true,
+				Default:     stringdefault.StaticString("bytes"),
+				Description: "Configures how events are decoded from raw bytes",
+				Validators: []validator.String{
+					stringvalidator.OneOf("bytes", "json"),
+				},
+			},
+			"connection_string": schema.StringAttribute{
+				Required:    true,
+				Description: "The Connection String as it appears in hub consumer SAS Policy",
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+					stringvalidator.LengthAtMost(512),
+				},
+			},
+			"namespace": schema.StringAttribute{
+				Required:    true,
+				Description: "The Event Hub Namespace",
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+					stringvalidator.LengthAtMost(256),
+				},
+			},
+			"group_id": schema.StringAttribute{
+				Required:    true,
+				Description: "The consumer group name that this consumer belongs to.",
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+					stringvalidator.LengthAtMost(256),
+				},
+			},
+			"topics": schema.ListAttribute{
+				Required:    true,
+				ElementType: StringType,
+				Description: "The list of Azure Event Hub name(s) to read events from.",
+				Validators: []validator.List{
+					listvalidator.SizeAtLeast(1),
+					listvalidator.ValueStringsAre(
+						stringvalidator.LengthAtLeast(1),
+						stringvalidator.LengthAtMost(256),
+					),
+				},
+			},
+		}, nil),
+	}
+}
+
+func AzureEventHubSourceFromModel(plan *AzureEventHubSourceModel, previousState *AzureEventHubSourceModel) (*Source, diag.Diagnostics) {
+	dd := diag.Diagnostics{}
+
+	component := Source{
+		BaseNode: BaseNode{
+			Type:        "azure-event-hub",
+			Title:       plan.Title.ValueString(),
+			Description: plan.Description.ValueString(),
+			UserConfig: map[string]any{
+				"decoding_codec":    plan.Decoding.ValueString(),
+				"connection_string": plan.ConnectionString.ValueString(),
+				"namespace":         plan.Namespace.ValueString(),
+				"group_id":          plan.GroupId.ValueString(),
+				"topics":            StringListValueToStringSlice(plan.Topics),
+			},
+		},
+	}
+
+	if previousState != nil {
+		component.Id = previousState.Id.ValueString()
+		component.GenerationId = previousState.GenerationId.ValueInt64()
+	}
+
+	return &component, dd
+}
+
+func AzureEventHubSourceToModel(plan *AzureEventHubSourceModel, component *Source) {
+	plan.Id = StringValue(component.Id)
+	if component.Title != "" {
+		plan.Title = StringValue(component.Title)
+	}
+	if component.Description != "" {
+		plan.Description = StringValue(component.Description)
+	}
+	plan.GenerationId = Int64Value(component.GenerationId)
+	plan.Decoding = StringValue(component.UserConfig["decoding_codec"].(string))
+	plan.ConnectionString = StringValue(component.UserConfig["connection_string"].(string))
+	plan.Namespace = StringValue(component.UserConfig["namespace"].(string))
+	plan.GroupId = StringValue(component.UserConfig["group_id"].(string))
+	plan.Topics = SliceToStringListValue(component.UserConfig["topics"].([]any))
+}
