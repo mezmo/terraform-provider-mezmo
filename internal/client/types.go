6b68c4004f1544949af58691e0ec3caa2c8e19fb
Chris Nixon <chris.nixon@logdna.com>
2023-12-06 23:36:20 +0000

    fix: fix request serialization
    Terraform's StringNull type seems to defeat Go’s built in json.Marshal's
    omitempty annotation.
    Add custom serialization logic to set fields which are not allowed to
    contain empty strings to `nil` so that encoding/json.Marshal can
    correctly ignore them
    
    Ref: LOG-18750

--- internal/client/types.go
+++ internal/client/types.go
@@ -1,8 +1,14 @@
 package client
 
 import (
+	"encoding/json"
+	"slices"
 	"time"
 )
+
+var DISALLOWED_EMPTY_STRING_FIELDS_BY_PARSER = map[string][]string{
+	"parse_key_value": {"field_delimiter", "key_delimiter"},
+}
 
 // Represents a Pipeline.
 type Pipeline struct {
@@ -20,6 +26,51 @@ type BaseNode struct {
 	Description  string         `json:"description,omitempty"`
 	UserConfig   map[string]any `json:"user_config"`
 	GenerationId int64          `json:"generation_id"`
+}
+
+func filterEmptyStrings(m map[string]any) map[string]any {
+	parserName, ok := m["parser"].(string)
+	if !ok {
+		return m
+	}
+	filteredFields, ok := DISALLOWED_EMPTY_STRING_FIELDS_BY_PARSER[parserName]
+	if !ok {
+		return m
+	}
+	userOptions, _ := m["options"].(map[string]any)
+	for key, val := range userOptions {
+		if slices.Contains(filteredFields, key) {
+			switch v := val.(type) {
+			case string:
+				if v == "" {
+					delete(userOptions, key)
+				}
+			}
+		}
+	}
+	if userOptions != nil {
+		m["options"] = userOptions
+	}
+	return m
+}
+
+func (u *BaseNode) MarshalJSON() ([]byte, error) {
+	u.UserConfig = filterEmptyStrings(u.UserConfig)
+
+	parsers, _ := u.UserConfig["parsers"].([]map[string]any)
+	for _, parserAttrs := range parsers {
+		filterEmptyStrings(parserAttrs)
+	}
+	if parsers != nil {
+		u.UserConfig["parsers"] = parsers
+	}
+
+	type Alias BaseNode
+	return json.Marshal(&struct {
+		*Alias
+	}{
+		Alias: (*Alias)(u),
+	})
 }
 
 type Source struct {
